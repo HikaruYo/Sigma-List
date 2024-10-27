4ee115712b0fc8d423642e60c390f24d15fb1af7
HikaruYo <adithaean@gmail.com>
2024-10-28 06:50:54 +0700

    Setup for Header (done)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,9 @@
-<div class="container py-16 items-center text-red-700">
-    <h2 class="text-red-800 ">Sigma List</h2>
-    <h3>Login</h3>
-</div>
+<section class="p-5 lg:px-52 md:px-20 sm:px-12 px-8 bg-gray-600">
+    <div class="container flex mx-auto justify-between items-center">
+        <h2 class="font-semibold text-3xl text-white">Sigma List</h2>
+        <div class="flex gap-8 items-center text-center">
+            <a href="" class="text-xl font-medium text-white">Login</a>
+            <a href="" class="text-xl font-medium text-white">Sign Up</a>
+        </div>
+    </div>
+</section>

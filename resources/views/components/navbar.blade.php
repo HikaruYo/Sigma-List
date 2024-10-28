c9b22b5fa185a6cad909e7e5034292cc7e3ba5b5
HikaruYo <adithaean@gmail.com>
2024-10-28 15:13:13 +0700

    Update Search Bar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,1 +1,11 @@
-s
+<section class="p-3 lg:px-52 md:px-20 sm:px-12 px-8">
+    <div class="flex justify-between">
+        <div class="flex gap-4">
+            <p>Top Anime</p>
+            <p>Genre</p>
+        </div>
+        <div>
+            Search Anime
+        </div>
+    </div>
+</section>

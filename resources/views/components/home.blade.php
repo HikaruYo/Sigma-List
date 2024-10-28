f4ead0cf3a31b8b49aa32fe6c43d1c76c39198e9
HikaruYo <adithaean@gmail.com>
2024-10-28 07:59:15 +0700

    Setup for Home Page

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sigma List</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    @include('components.header')
+<section class="p-5 lg:px-52 md:px-20 sm:px-12 px-8 bg-gray-400">
+    <div class="h-full justify-center items-center text-center">
+        <h2 class="text-2xl font-semibold">Anime Databases for You</h2>
 
-</body>
-</html>
+    </div>
+</section>

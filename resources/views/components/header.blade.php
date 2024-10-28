09a879e50dca01cbc0b109538ff47cc48d2cc5e2
HikaruYo <adithaean@gmail.com>
2024-10-28 14:55:40 +0700

    Update Header and Setup for Search Bar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,9 @@
-<section class="p-5 lg:px-52 md:px-20 sm:px-12 px-8 bg-gray-600">
+<section class="p-3 lg:px-52 md:px-20 sm:px-12 px-8 bg-slate-900">
     <div class="container flex mx-auto justify-between items-center">
-        <h2 class="font-semibold text-3xl text-white">Sigma List</h2>
-        <div class="flex gap-8 items-center text-center">
-            <a href="" class="text-xl font-medium text-white">Login</a>
-            <a href="" class="text-xl font-medium text-white">Sign Up</a>
+        <h2 class="font-semibold md:text-3xl text-xl text-white">Sigma List</h2>
+        <div class="flex md:gap-3 gap-1 items-center text-center">
+            <a href="" class="w-28 border-cyan-200 bg-gray-700 px-3 rounded-md md:text-xl text-md font-medium hover:bg-gray-400 transition duration-500">Login</a>
+            <a href="" class="w-28 border-cyan-200 bg-cyan-500 px-3 rounded-md md:text-xl text-md font-medium hover:bg-cyan-200 transition duration-500">Sign Up</a>
         </div>
     </div>
 </section>
--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -10,7 +10,7 @@
 <body>
 
     @include('components.header')
-
+    @include('components.navbar')
     @include('components.home')
 
 </body>

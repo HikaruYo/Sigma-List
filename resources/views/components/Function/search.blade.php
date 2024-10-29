a2d1a2517c05ec81ff847e582fe575f7d4e8ee09
HikaruYo <adithaean@gmail.com>
2024-10-29 07:59:20 +0700

    Make the UI for Search Bar

---
+++ resources/views/components/Function/search.blade.php
@@ -0,0 +1,14 @@
+<div class="flex items-center bg-gray-100 rounded-full px-4 py-1 w-96 max-w-lg mx-auto shadow-md">
+    <input
+        type="text"
+        placeholder="Search Anime"
+        class="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-500"
+    />
+    <button
+        class="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-1.5 transition duration-300"
+    >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M11 4a7 7 0 100 14 7 7 0 000-14z" />
+        </svg>
+    </button>
+</div>

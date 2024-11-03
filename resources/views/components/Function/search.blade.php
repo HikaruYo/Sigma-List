a3044c7090e8c653d55e26df3cfbd53876a2253c
HikaruYo <adithaean@gmail.com>
2024-11-03 14:45:05 +0700

    Add Category

--- resources/views/components/Function/search.blade.php
+++ resources/views/components/Function/search.blade.php
@@ -10,7 +10,7 @@
     <!-- Bagian button search -->
     <div class="flex items-center justify-center py-1 hover:bg-gray-500 text-gray-700 border-l-2 border-gray-700 px-1 rounded-r-xl transition duration-300">
         <button
-            class=""
+            class="pb-0.5"
         >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M11 4a7 7 0 100 14 7 7 0 000-14z" />
--- resources/views/components/search-box.blade.php
+++ resources/views/components/search-box.blade.php
@@ -4,9 +4,19 @@
             {{-- TODO: Make Search Engine --}}
             @include('components.Function.search')
         </div>
-        <div class="flex pt-4 gap-4 font-medium">
-            <a href="#top-anime">Top Anime</a>
-            <a href="#category">Category</a>
+        <div class="flex flex-col px-2 pt-4 w-full font-medium">
+            <p>Category</p>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-2 p-2 border-2 border-black rounded-lg mt-2 bg-gray-300 text-sm">
+                <a href="" class="hover:text-gray-400 transition duration-200 border-b-2 border-black text-center pb-1">Top Anime</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 border-b-2 border-black text-center pb-1">Movie</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 border-b-2 border-black text-center pb-1">Action</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 border-b-2 border-black text-center pb-1">Fantasy</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 border-b-2 border-black text-center pb-1">Romance</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 border-b-2 border-black text-center pb-1">Slice of Life</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 text-center">Comedy</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 text-center">Drama</a>
+                <a href="" class="hover:text-gray-400 transition duration-200 text-center">Horror</a>
+            </div>
         </div>
     </div>
 </section>

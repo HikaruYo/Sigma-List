0de4fc2fc76dea75534fe1359d0cdb6b1e2881d7
HikaruYo <adithaean@gmail.com>
2024-11-04 19:52:12 +0700

    Setup for Login Page (@VentDN buat woy login page)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,8 +2,8 @@
     <div class="container flex mx-auto justify-between items-center">
         <h2 class="font-semibold md:text-3xl text-xl text-white">Sigma List</h2>
         <div class="flex md:gap-3 gap-1 items-center text-center">
-            <a href="" class="w-28 border-cyan-200 bg-gray-700 px-3 rounded-md md:text-xl text-md font-medium hover:bg-gray-400 transition duration-500">Login</a>
-            <a href="" class="w-28 border-cyan-200 bg-cyan-500 px-3 rounded-md md:text-xl text-md font-medium hover:bg-cyan-200 transition duration-500">Sign Up</a>
+            <a href="/login" class="w-28 border-cyan-200 bg-gray-700 px-3 rounded-md md:text-xl text-md font-medium hover:bg-gray-400 transition duration-500">Login</a>
+            <a href="/signup" class="w-28 border-cyan-200 bg-cyan-500 px-3 rounded-md md:text-xl text-md font-medium hover:bg-cyan-200 transition duration-500">Sign Up</a>
         </div>
     </div>
 </section>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('components.main');
 });
+
+Route::get('/login', function () {
+   return view('components.Login.login-page');
+});

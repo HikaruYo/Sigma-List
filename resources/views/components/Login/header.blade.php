5f1dfed3266bc61f1629a122d10a3f691b8c79ac
HikaruYo <adithaean@gmail.com>
2024-11-10 08:19:47 +0700

    Adding Custom Font

---
+++ resources/views/components/Login/header.blade.php
@@ -0,0 +1,9 @@
+<section class="p-3 lg:px-52 md:px-20 sm:px-12 px-8 bg-[#0e1111]">
+    <div class="container flex mx-auto justify-between items-center">
+        <a href="/" class="font-semibold md:text-3xl text-xl text-white">Sigma List</a>
+        <div class="flex md:gap-3 gap-1 items-center text-center">
+{{--            <a href="/login" class="w-28 border-cyan-200 bg-gray-700 px-3 rounded-md md:text-xl text-md font-medium hover:bg-gray-400 transition duration-500">Login</a>--}}
+            <a href="/signup" class="w-28 border-cyan-200 bg-cyan-500 px-3 rounded-md md:text-xl text-md font-medium hover:bg-cyan-200 transition duration-500">Sign Up</a>
+        </div>
+    </div>
+</section>
--- resources/views/components/Login/login-page.blade.php
+++ resources/views/components/Login/login-page.blade.php
@@ -9,9 +9,25 @@
     @vite('resources/css/app.css')
 </head>
 <body class="">
+    @include('components.Login.header')
 {{-- TODO: BUAT LOGIN PAGE @UnggasHitamDariSukuPedalaman !! --}}
-    <div class="bg-[#232b2b]">
-        p
+    <div class="bg-[#232b2b] flex flex-col items-center justify-center">
+        <h2 class="text-3xl font-bold text-white">Login</h2>
+        <form class="flex flex-col p-4 gap-2">
+            @csrf
+            <input
+                type="email"
+                placeholder="Email"
+                class="p-1.5 rounded-lg"
+                required
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                class="p-1.5 rounded-lg"
+                required
+            />
+        </form>
     </div>
 </body>
 </html>

348fb90d1a10c4931ae5a059a6427d743571548b
HikaruYo <adithaean@gmail.com>
2024-11-10 08:39:48 +0700

    Login Page Done (maybe)

--- resources/views/components/Login/login-page.blade.php
+++ resources/views/components/Login/login-page.blade.php
@@ -8,26 +8,35 @@
     <title>Login</title>
     @vite('resources/css/app.css')
 </head>
-<body class="">
+<body class="bg-[#232b2b]">
     @include('components.Login.header')
-{{-- TODO: BUAT LOGIN PAGE @UnggasHitamDariSukuPedalaman !! --}}
-    <div class="bg-[#232b2b] flex flex-col items-center justify-center">
-        <h2 class="text-3xl font-bold text-white">Login</h2>
-        <form class="flex flex-col p-4 gap-2">
-            @csrf
-            <input
-                type="email"
-                placeholder="Email"
-                class="p-1.5 rounded-lg"
-                required
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                class="p-1.5 rounded-lg"
-                required
-            />
-        </form>
+
+    <div class="flex items-center justify-center m-20">
+        {{-- TODO: BUAT LOGIN PAGE @UnggasHitamDariSukuPedalaman !! --}}
+        <div class="bg-gray-600 w-fit p-6 flex flex-col items-center justify-center shadow-2xl rounded-xl">
+            <h2 class="text-3xl font-bold text-white">Login</h2>
+            <form method="post" class="flex flex-col p-4 gap-2">
+                @csrf
+                <input
+                    type="email"
+                    placeholder="Email"
+                    class="p-1.5 rounded-lg"
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    class="p-1.5 rounded-lg"
+                    required
+                />
+                <button
+                    type="submit"
+                    class="bg-black bg-opacity-60 rounded-lg text-white text-xl p-1 mt-6"
+                >
+                    Login
+                </button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
